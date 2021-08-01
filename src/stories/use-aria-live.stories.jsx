c78a0a3f21827eda2cd2938758c9337a4ca78000
Michael Chan <mijoch@gmail.com>
2021-07-31 19:38:55 -0700

    add story to prove integration

---
+++ src/stories/use-aria-live.stories.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useAriaLive, PoliteAriaLive, AssertiveAriaLive } from "../index";
+
+function Subject(props) {
+  let [politeAnnouncement, announcePolitely] = useAriaLive();
+  let [assertiveAnnouncement, announceAssertively] = useAriaLive();
+
+  return (
+    <>
+      <p>
+        The buttons below create messages that are only visible to users of
+        assitive technologies like screen readers.
+      </p>
+      <p>
+        Open the <strong>Aria Live Regions</strong> Panel tab to see announced
+        text.
+      </p>
+
+      <button
+        type="button"
+        onClick={() => announcePolitely("Item added, politely")}
+      >
+        Announce "Item added, politely".
+      </button>
+      <br />
+      <button
+        type="button"
+        onClick={() => announceAssertively("Item added, assertively")}
+      >
+        Announce "Item added, assertively".
+      </button>
+
+      <div
+        style={{
+          marginTop: "2rem",
+          backgroundColor: "#eee",
+          padding: "1rem",
+          borderRadius: ".25rem",
+        }}
+      >
+        See the current state of the announced value for debugging:
+        <pre>Polite: {politeAnnouncement}</pre>
+        <pre>Assertive: {assertiveAnnouncement}</pre>
+      </div>
+
+      <PoliteAriaLive>{politeAnnouncement}</PoliteAriaLive>
+      <AssertiveAriaLive>{assertiveAnnouncement}</AssertiveAriaLive>
+    </>
+  );
+}
+
+export default {
+  title: "AriaLiveRegion",
+  component: Subject,
+};
+
+const Template = (args) => <Subject {...args} />;
+
+export const Simple = Template.bind({});
+Simple.args = {};
